dd1a1a54629071bb7708e5e292deeb4710109eba
Sanjay Prajapati <sanjay270899@gmail.com>
2020-12-29 15:17:52 +0530

    Header Added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      start working here
+      <Header />
     </div>
   );
 }
