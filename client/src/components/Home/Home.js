801297e2190cc86f2a4444d2f973afa2380ce1d0
Sanjay Prajapati <sanjay270899@gmail.com>
2021-01-24 00:13:37 +0530

    Home Page Added

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Main/Main';
+import Predictions from './Predictions/Predictions';
+
+function Home() {
+    return (
+        <>
+            <Main />
+            <Predictions />
+        </>
+    )
+}
+
+export default Home
