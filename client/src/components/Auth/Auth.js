4d7bc1be8e97521f3c6c10108ac1f10eb61f3b57
Sanjay Prajapati <sanjay270899@gmail.com>
2021-01-27 19:50:03 +0530

    Auth Page Created

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Auth.module.css';
+import SignIn from './SignIn/SignIn';
+import SignUp from './SignUp/SignUp';
+import Logo from '../../assets/Logo.png';
+
+function Auth({option = false}) {
+    return (
+        <div className={styles.Auth}>
+            <div className={styles.left}>
+                <img src={Logo} alt="Logo" className={styles.logo}/>
+                <div className={styles.title}>What is Realate?</div>
+                <div className={styles.content}>Real-time real-estate data prediction engine</div>
+            </div>
+            <div className={styles.right}>
+                {
+                    option ?
+                    <SignIn />
+                    :
+                    <SignUp />
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Auth
