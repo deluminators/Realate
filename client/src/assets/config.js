da81ffca566993c012ecb509750d81fb89b734b1
Sanjiban Sengupta <sanjiban.sg@gmail.com>
2021-10-18 23:46:30 +0530

    fix: Requirements file & Flask app config (with updations in frontend & backend) (#68)
    * update: backend with changes in classification & prediction modules
    
    * update: frontend with changes in classification & prediction modules
    
    * fix: requirements file & flask app config
    
    * refactor frontend as suggested by @07souravkunda

--- client/src/assets/config.js
+++ client/src/assets/config.js
@@ -1,5 +1,4 @@
 export default {
-    // LINK: "https://calm-anchorage-30458.herokuapp.com/api/v1",
-    LINK: "http://localhost:5000/api/v1",
-  };
-  
+  // LINK: "https://calm-anchorage-30458.herokuapp.com/api/v1",
+  LINK: 'http://localhost:3000/api/v1',
+};
